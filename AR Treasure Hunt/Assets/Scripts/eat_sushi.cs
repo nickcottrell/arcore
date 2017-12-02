f6844ca6c849129c402b1f120f2bf0435cb20d5f
nickcottrell <nick.a.cottrell@gmail.com>
2017-12-02 14:25:30 -0800

    adding treasure hunt project

---
+++ AR Treasure Hunt/Assets/Scripts/eat_sushi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eat_sushi : MonoBehaviour {
+
+	public GameObject edibleObject;
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject == edibleObject)
+		{
+			Destroy(edibleObject);
+		}
+	}
+}
